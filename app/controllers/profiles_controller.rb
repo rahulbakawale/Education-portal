c19aa4ec1d139743eeae32ebc8a0bbb764bd2e17
Bakawale <bakawale51@gmail.com>
2020-10-13 15:09:18 +0530

    Using for the URL change name path

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,28 +1,35 @@
 class ProfilesController < ApplicationController
 	skip_before_action :require_submit_profile
 	before_action :profile_find, only: [:show, :edit, :update]
-	before_action :add_profile, only: [:show, :edit, :update]
+	before_action :get_profile, only: [:show, :edit, :update]
+	#before_action only: [:edit]
 
 
 	def index
-			@profiles  = Profile.all
+		@profiles  = Profile.all
 		end
 
 	def new
-		  @profile = current_user.build_profile
+		@profile = current_user.build_profile
 	  end
 
 	def edit
-    	#@profile = Profile.find(params[:id])
+		#@profile = Profile.find(params[:id])
+		#unless(params[:id].to_i === current_user.profile.id)
+		#   redirect_to profiles_path 
+		#   flash[:alert] = "You are not authrized to edit"
+		#end
+	 	#@profile = current_user.profile	
    	end
 
+	
 	def show
 	  	#@profile = Profile.find(params[:id])
 		end
 
 
 	def create
-   #	debugger
+   		#debugger
 	 	profile = current_user.build_profile(profile_params)
 	 	if (profile.save)
 	 	redirect_to profile_path(profile.id)
@@ -30,6 +37,7 @@ class ProfilesController < ApplicationController
 	 	else
 	 	@profile = current_user.build_profile
 		flash[:alert]=profile.errors.full_messages
+		flash[:alert]="Some Errors"
 	    render :new
 	   end
 	end
@@ -48,16 +56,39 @@ class ProfilesController < ApplicationController
 
 	
 	private
-			def profile_find
-				@profile = Profile.find(params[:id])
-			end
 
-		 	def add_profile
-		 	    user_signed_in? == current_user
-		 	end
+	
+	def profile_find
+		@profile = Profile.find(params[:id])
+	end
 
-    	def  profile_params
-    		params.require(:profile).permit(:first_name, :last_name, :contact_num, :address, :city, :pin_code, :image)
-    	end
+	def get_profile
+		#debugger
+		unless(params[:id].to_i === current_user.profile.id)
+	   redirect_to profiles_path 
+	   flash[:alert] = "You are not authrized to edit"
+		end
+ 		@profile = current_user.profile
+
+	  end
+
+
+	def 
+	 profile_params
+		params.require(:profile).permit(:first_name, :last_name, :contact_num, :address, :city, :pin_code, :image)
+	end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
