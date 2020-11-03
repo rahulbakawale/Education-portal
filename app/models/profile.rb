d647ef29540863ee426c6299a9f4f56cd06f57c4
Bakawale <bakawale51@gmail.com>
2020-11-03 11:42:24 +0530

    Some changes for like and review and comment page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,6 +5,6 @@ class Profile < ApplicationRecord
 #    validates :first_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 #	validates :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 validates :first_name, :last_name, :city, :contact_num, :presence => true
-#
+	
 	
 end
